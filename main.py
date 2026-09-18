76a0232ae30fa76be9ec29f231746753f4968353
Claudio <claudiofsantos05@gmail.com>
2026-09-18 17:17:24 -0300

    seviraai

---
+++ main.py
@@ -0,0 +1,10 @@
+def eunaosei(lado):
+    return lado * 4
+
+entrada = input("digite um valor: ")
+
+valor_digitado = float(entrada)
+
+resultado = eunaosei(valor_digitado)
+
+print(f"o resultado é: {resultado}")
